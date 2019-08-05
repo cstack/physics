52e58b13d0d59f0c42a004a157544264be65d546
Connor Stack <sirconnorstack@gmail.com>
2019-08-05 08:40:51 -0700

    Bouncing off walls and each other

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+var SIMULATION_SPEED = 1;
 var Entity = /** @class */ (function () {
     function Entity(x, y, vx, vy, radius) {
         this.x = x;
@@ -27,15 +28,17 @@ function init() {
     ctx = c;
     state = {
         entities: [
-            new Entity(0, 0, 20, 0, 10),
-            new Entity(100, 0, -10, 0, 20),
+            new Entity(0, 10, 500, 0, 10),
+            new Entity(100, 0, -1000, 0, 20),
         ],
         lastRender: 0,
+        paused: false,
+        t: 0,
     };
     mainLoop(0);
 }
 function mainLoop(timestamp) {
-    if (timestamp > 10000) {
+    if (timestamp > 10000 || state.paused) {
         return;
     }
     else {
@@ -46,7 +49,8 @@ function mainLoop(timestamp) {
     }
     var dt = (timestamp - state.lastRender) / 1000;
     state.lastRender = timestamp;
-    simulate(dt);
+    state.t = timestamp;
+    simulate(dt * SIMULATION_SPEED);
     renderFrame();
 }
 function simulate(dt) {
@@ -59,11 +63,43 @@ function simulate(dt) {
         // update position
         entity.x += entity.vx * dt;
         entity.y += entity.vy * dt;
-        // bounce
+        // bounce off walls
         if (entity.y > 500) {
             entity.y = 500;
             entity.vy *= -0.90;
         }
+        if (entity.x > 500) {
+            entity.x = 500;
+            entity.vx *= -0.90;
+        }
+        if (entity.x < 0) {
+            entity.x = 0;
+            entity.vx *= -0.90;
+        }
+        // bounce off each other
+        state.entities.forEach(function (other) {
+            if (entity === other) {
+                return;
+            }
+            var d = distance(entity, other);
+            var minD = entity.radius + other.radius;
+            if (d < minD) {
+                var towardOther = direction(entity, other);
+                var displacement = minD - d;
+                var displacementPerEntity = displacement / 2;
+                entity.x -= towardOther.x * displacementPerEntity;
+                entity.y -= towardOther.y * displacementPerEntity;
+                other.x += towardOther.x * displacementPerEntity;
+                other.y += towardOther.y * displacementPerEntity;
+                var tmpVx = entity.vx;
+                var tmpVy = entity.vy;
+                entity.vx = other.vx;
+                entity.vy = other.vy;
+                other.vx = tmpVx;
+                other.vy = tmpVy;
+            }
+            // state.paused = true;
+        });
     });
 }
 function renderFrame() {
@@ -78,6 +114,15 @@ function renderFrame() {
         });
         drawCircle(ctx, { x: entity.x, y: entity.y, radius: entity.radius, color: color });
     });
+    for (var i = 0; i < state.entities.length; i++) {
+        for (var j = i + 1; i < state.entities.length; j++) {
+            var e1 = state.entities[i];
+            var e2 = state.entities[j];
+            var normalVector = direction(e1, e2);
+            var line = scale(normalVector, 10);
+            drawLine(ctx, e1.x, e1.y, e1.x + line.x, e1.y + line.y);
+        }
+    }
 }
 function drawCircle(ctx, params) {
     ctx.beginPath();
@@ -85,8 +130,27 @@ function drawCircle(ctx, params) {
     ctx.arc(params.x, params.y, params.radius, 0, Math.PI * 2, true);
     ctx.fill();
 }
+function drawLine(ctx, x1, y1, x2, y2) {
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
 function overlapping(entity1, entity2) {
-    var distance = Math.sqrt(Math.pow((entity1.x - entity2.x), 2) + Math.pow((entity1.y - entity2.y), 2));
-    return distance < (entity1.radius + entity2.radius);
+    var d = distance(entity1, entity2);
+    return d < (entity1.radius + entity2.radius);
+}
+function distance(entity1, entity2) {
+    return Math.sqrt(Math.pow((entity1.x - entity2.x), 2) + Math.pow((entity1.y - entity2.y), 2));
+}
+function direction(from, to) {
+    var dx = to.x - from.x;
+    var dy = to.y - from.y;
+    var d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    return { x: dx / d, y: dy / d };
+}
+function scale(vector, scale) {
+    return { x: vector.x * scale, y: vector.y * scale };
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+const SIMULATION_SPEED = 1;
+
 class Entity {
   x: number;
   y: number;
@@ -19,10 +21,17 @@ type Color = {
   b: number;
 };
 
+type Vector = {
+  x: number,
+  y: number,
+}
+
 let ctx: CanvasRenderingContext2D;
 var state: {
   entities: Entity[],
   lastRender: number,
+  paused: boolean,
+  t: number,
 };
 
 function init() {
@@ -41,17 +50,19 @@ function init() {
   ctx = c;
   state = {
     entities: [
-      new Entity(0, 0, 20, 0, 10),
-      new Entity(100, 0, -10, 0, 20),
+      new Entity(0, 10, 500, 0, 10),
+      new Entity(100, 0, -1000, 0, 20),
     ],
     lastRender: 0,
+    paused: false,
+    t: 0,
   }
 
   mainLoop(0);
 }
 
 function mainLoop(timestamp: number) {
-  if (timestamp > 10000) {
+  if (timestamp > 10000 || state.paused) {
     return;
   } else {
     requestAnimationFrame(mainLoop);
@@ -63,7 +74,8 @@ function mainLoop(timestamp: number) {
   var dt = (timestamp - state.lastRender)/1000;
   state.lastRender = timestamp;
 
-  simulate(dt);
+  state.t = timestamp;
+  simulate(dt * SIMULATION_SPEED);
   renderFrame();
 }
 
@@ -80,11 +92,44 @@ function simulate(dt: number) {
     entity.x += entity.vx*dt;
     entity.y += entity.vy*dt;
 
-    // bounce
+    // bounce off walls
     if (entity.y > 500) {
       entity.y = 500;
       entity.vy *= -0.90;
-    } 
+    }
+    if (entity.x > 500) {
+      entity.x = 500;
+      entity.vx *= -0.90;
+    }
+    if (entity.x < 0) {
+      entity.x = 0;
+      entity.vx *= -0.90;
+    }
+
+    // bounce off each other
+    state.entities.forEach((other) => {
+      if (entity === other) {
+        return;
+      }
+      let d = distance(entity, other);
+      let minD = entity.radius + other.radius;
+      if (d < minD) {
+        let towardOther = direction(entity, other);
+        let displacement = minD - d;
+        let displacementPerEntity = displacement/2;
+        entity.x -= towardOther.x*displacementPerEntity;
+        entity.y -= towardOther.y*displacementPerEntity;
+        other.x += towardOther.x*displacementPerEntity;
+        other.y += towardOther.y*displacementPerEntity;
+        let tmpVx = entity.vx;
+        let tmpVy = entity.vy;
+        entity.vx = other.vx;
+        entity.vy = other.vy;
+        other.vx = tmpVx;
+        other.vy = tmpVy;
+      }
+      // state.paused = true;
+    })
   });
 }
 
@@ -99,7 +144,16 @@ function renderFrame() {
       }
     });
     drawCircle(ctx, {x: entity.x, y: entity.y, radius: entity.radius, color: color});
-  })
+  });
+  for (let i = 0; i < state.entities.length; i++) {
+    for (let j = i+1; i < state.entities.length; j++) {
+      let e1 = state.entities[i];
+      let e2 = state.entities[j];
+      let normalVector = direction(e1, e2);
+      let line = scale(normalVector, 10);
+      drawLine(ctx, e1.x, e1.y, e1.x + line.x, e1.y + line.y);
+    }
+  }
 }
 
 function drawCircle(ctx: CanvasRenderingContext2D, params: {x: number, y:number, radius: number, color: Color}) {
@@ -109,7 +163,30 @@ function drawCircle(ctx: CanvasRenderingContext2D, params: {x: number, y:number,
   ctx.fill();
 }
 
+function drawLine(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+  ctx.fillStyle = 'black';
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+}
+
 function overlapping(entity1: Entity, entity2: Entity) {
-  let distance: number = Math.sqrt((entity1.x-entity2.x)**2 + (entity1.y-entity2.y)**2);
-  return distance < (entity1.radius + entity2.radius);
+  let d: number = distance(entity1, entity2)
+  return d < (entity1.radius + entity2.radius);
+}
+
+function distance(entity1: Entity, entity2: Entity) {
+  return Math.sqrt((entity1.x-entity2.x)**2 + (entity1.y-entity2.y)**2);
+}
+
+function direction(from: Entity, to: Entity) {
+  let dx = to.x - from.x;
+  let dy = to.y - from.y;
+  let d = Math.sqrt(dx**2 + dy**2);
+  return {x: dx/d, y: dy/d};
+}
+
+function scale(vector: Vector, scale: number) {
+  return {x: vector.x*scale, y: vector.y*scale}
 }
